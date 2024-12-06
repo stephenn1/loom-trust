0935cc130f3197132b96b0d724f59263006ca708
stephenn1 <okonkwostephen79@gmail.com>
2024-12-06 13:47:55 +0100

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,17 @@ import AboutUsSection from "@/components/web-site/home-page/about-us-section";
 import HeroSection from "@/components/web-site/home-page/hero-section";
 import MarketOverview from "@/components/web-site/home-page/market-overview";
 import OurServicesSection from "@/components/web-site/home-page/our-services-section";
+import Partners from "@/components/web-site/home-page/partners-logo";
+import Statistics from "@/components/web-site/home-page/statistics";
 
 export default function Home() {
   return (
     <main className="w-full h-full grid">
       <HeroSection />
+      <Statistics />
       <AboutUsSection />
       <OurServicesSection />
+      <Partners />
       <MarketOverview />
     </main>
   );
